8596059e67f98c113fbb775e0918f070ad2c8fda
hjalteklarsen <hjalteklarsen@gmail.com>
2025-05-01 11:09:58 +0200

    initial

---
+++ src/test/java/org/example/bilabonnement/BilAbonnementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bilabonnement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilAbonnementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
