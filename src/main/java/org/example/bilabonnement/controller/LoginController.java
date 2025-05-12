d0256128dd1c4a9e5e76896f22055b58c00f6c4d
Valdemar Støvring Sørensen <vsorensen31@gmail.com>
2025-05-12 13:39:22 +0200

    login fix

--- src/main/java/org/example/bilabonnement/controller/LoginController.java
+++ src/main/java/org/example/bilabonnement/controller/LoginController.java
@@ -1,14 +1,49 @@
 package org.example.bilabonnement.controller;
 
+import org.example.bilabonnement.model.user.User;
+import org.example.bilabonnement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
+    @Autowired
+    private UserService userService;
 
     @GetMapping("/login")
     public String login() {
         return "login";
     }
+
+    @PostMapping("/login")
+    public String processLogin(@RequestParam String username,
+                               @RequestParam String password,
+                               Model model) {
+        User user = userService.authenticate(username, password);
+
+        if (user != null) {
+            model.addAttribute("user", user);
+
+            System.out.println(user.getRole());
+
+            return switch (user.getRole()) {
+                case "DATA" -> "data-dashboard";
+                case "SKADE" -> "damage-dashboard";
+                case "UDVIKLING" -> "business-dashboard";
+                default -> {
+                    model.addAttribute("error", "Ukendt rolle.");
+                    yield "login";
+                }
+            };
+        } else {
+            model.addAttribute("error", "Forkert brugernavn eller adgangskode");
+            return "login";
+        }
+    }
+
 }
