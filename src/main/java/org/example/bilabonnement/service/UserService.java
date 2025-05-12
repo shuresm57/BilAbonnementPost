3ff84a74fc48a09347ab9ff0e4434db0f7eedea3
Valdemar Støvring Sørensen <vsorensen31@gmail.com>
2025-05-12 10:42:28 +0200

    fetch users

--- src/main/java/org/example/bilabonnement/service/UserService.java
+++ src/main/java/org/example/bilabonnement/service/UserService.java
@@ -1,8 +1,30 @@
 package org.example.bilabonnement.service;
 
 
+import org.example.bilabonnement.model.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.*;
 
 @Service
 public class UserService {
+
+    @Autowired
+    JdbcTemplate template;
+
+    //Vi bruger datatypen Set, så vi er sikre på at der ikke er nogen duplikater
+    public Set<User> fetchAllUsers() {
+        String sql = "SELECT * FROM user";
+        RowMapper<User> rowMapper = new BeanPropertyRowMapper<>(User.class);
+        List<User> users = template.query(sql, rowMapper);
+
+        //Sorterer listen via username
+        Set<User> sortedUsers = new TreeSet<>(Comparator.comparing(User::getUsername));
+        sortedUsers.addAll(users);
+        return sortedUsers;
+    }
 }
