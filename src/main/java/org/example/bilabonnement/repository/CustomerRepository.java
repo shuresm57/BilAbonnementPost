ab116b0d881d6bf5b4a6309c33df5fede19b03c6
hjalteklarsen <hjalteklarsen@gmail.com>
2025-05-13 09:39:04 +0200

    customer repo

--- src/main/java/org/example/bilabonnement/repository/CustomerRepository.java
+++ src/main/java/org/example/bilabonnement/repository/CustomerRepository.java
@@ -1,8 +1,47 @@
 package org.example.bilabonnement.repository;
 
 
+import org.example.bilabonnement.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class CustomerRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public List<Customer> fetchAll(){
+        String sql = """
+    SELECT 
+        customer_id,
+        fname AS first_name,
+        lname AS last_name,
+        phone_no,
+        license_no,
+        street_name,
+        zip,
+        email
+    FROM customer
+""";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Customer.class));
+    }
+    public void addCustomer(Customer customer) {
+        String sql = "INSERT INTO customer (customer_id, fname, lname, email, phone_no, license_no, street_name, zip) " +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        jdbcTemplate.update(sql,
+                customer.getCustomerId(),
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail(),
+                customer.getPhone(),
+                customer.getLicenseNo(),
+                customer.getStreetName(),
+                customer.getZip());
+    }
 }
--- src/main/java/org/example/bilabonnement/service/CustomerService.java
+++ src/main/java/org/example/bilabonnement/service/CustomerService.java
@@ -1,8 +1,25 @@
 package org.example.bilabonnement.service;
 
 
+import org.example.bilabonnement.model.Customer;
+import org.example.bilabonnement.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
+@Autowired
+    private CustomerRepository customerRepository;
+
+public List<Customer> fetchAll()
+{
+    return customerRepository.fetchAll();
+}
+
+public void addCustomer(Customer customer)
+{
+    customerRepository.addCustomer(customer);
+}
 }
