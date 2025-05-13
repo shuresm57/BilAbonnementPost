e870bb29ebf7db146f509ac3ea0f1dbbbd37dccf
NikoZK <nikodem09@live.dk>
2025-05-13 12:08:34 +0200

    Skade skade til Damage damage

--- src/main/java/org/example/bilabonnement/model/Damage.java
+++ src/main/java/org/example/bilabonnement/model/Damage.java
@@ -1,4 +1,60 @@
 package org.example.bilabonnement.model;
 
 public class Damage {
+
+    private int damage_id;
+    private String description;
+    private double price;
+    private String img_url;
+    private int report_id;
+
+    public Damage() {}
+
+    public Damage(int damage_id, String description, double price, String img_url, int report_id) {
+        this.damage_id = damage_id;
+        this.description = description;
+        this.price = price;
+        this.img_url = img_url;
+        this.report_id = report_id;
+    }
+
+    public int getDamage_id() {
+        return damage_id;
+    }
+
+    public void setDamage_id(int damage_id) {
+        this.damage_id = damage_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getImg_url() {
+        return img_url;
+    }
+
+    public void setImg_url(String img_url) {
+        this.img_url = img_url;
+    }
+
+    public int getReport_id() {
+        return report_id;
+    }
+
+    public void setReport_id(int report_id) {
+        this.report_id = report_id;
+    }
 }
---
+++ src/main/java/org/example/bilabonnement/service/DamageService.java
@@ -0,0 +1,31 @@
+package org.example.bilabonnement.service;
+
+import org.example.bilabonnement.model.Damage;
+import org.example.bilabonnement.repository.DamageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DamageService {
+
+    @Autowired
+    private DamageRepository damageRepository;
+
+    public void createDamage(Damage damage) {
+        damageRepository.createDamage(damage);
+    }
+
+    public void deleteDamage(Damage damage) {
+        damageRepository.deleteDamage(damage);
+    }
+
+    public void updateDamage(Damage damage) {
+        damageRepository.updateDamage(damage);
+    }
+
+    public List<Damage> getAllDamages() {
+        return damageRepository.fetchAllDamages();
+    }
+}
